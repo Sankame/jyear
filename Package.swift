270625a2bfc4607c1ff38e5e3dd669d2e4c13f8f
sankame <handycola3@gmail.com>
2016-12-10 17:43:04 +0000

    The below sample code worked.
    http://www.kitura.io/en/resources/tutorials/templating.html

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "jyear",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 1)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,13 +1,55 @@
 import Kitura
+import KituraMustache
+import KituraStencil
+import Foundation
 
 let router = Router()
 
-router.get("/") {
-    request, response, next in
-    response.send("jyear!")
-    next()
+router.add(templateEngine: MustacheTemplateEngine())
+router.add(templateEngine: StencilTemplateEngine())
+
+// Handle HTTP GET requests to GRMustache/
+router.get("/delivery") { request, response, next in
+    defer {
+        next()
+    }
+
+    // the example from https://github.com/groue/GRMustache.swift/blob/master/README.md
+    var context: [String: Any] = [
+        "name": "Arthur",
+        "date": Date(),
+        "realDate": Date().addingTimeInterval(60*60*24*3),
+        "late": true
+    ]
+
+    // Let template format dates with ``
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    context["format"] = dateFormatter
+
+    try response.render("document.mustache", context: context).end()
 }
 
+//Handle HTTP GET requests to Stencil
+router.get("/articles") { request, response, next in
+    defer {
+        next()
+    }
+
+    // the example from https://github.com/kylef/Stencil/blob/master/README.md
+    var context2 = [
+       "articles": [
+         ["title": "Migrating from OCUnit to XCTest", "author": "Kyle Fuller"],
+         ["title": "Memory Management with ARC", "author": "Kyle Fuller" ]
+       ]
+    ]
+
+    try response.render("document.stencil", context: context2).end()
+}
+
+// Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: 8090, with: router)
 
+// Start the Kitura runloop (this call never returns)
 Kitura.run()
+
