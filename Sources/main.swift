f723d53d2a078b6d8d9c575c327b7eccf90307db
sankame <handycola3@gmail.com>
2016-12-10 17:52:09 +0000

    I had the below error message, so I deleted "GRMustache".
    "support for GRMustache not yet implemented on Linux"

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,34 +1,10 @@
 import Kitura
-import KituraMustache
 import KituraStencil
 import Foundation
 
 let router = Router()
 
-router.add(templateEngine: MustacheTemplateEngine())
 router.add(templateEngine: StencilTemplateEngine())
-
-// Handle HTTP GET requests to GRMustache/
-router.get("/delivery") { request, response, next in
-    defer {
-        next()
-    }
-
-    // the example from https://github.com/groue/GRMustache.swift/blob/master/README.md
-    var context: [String: Any] = [
-        "name": "Arthur",
-        "date": Date(),
-        "realDate": Date().addingTimeInterval(60*60*24*3),
-        "late": true
-    ]
-
-    // Let template format dates with ``
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = .medium
-    context["format"] = dateFormatter
-
-    try response.render("document.mustache", context: context).end()
-}
 
 //Handle HTTP GET requests to Stencil
 router.get("/articles") { request, response, next in
