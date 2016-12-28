6159fc024ec86475bcf4f4d5ab0eaeb0fb5708cf
sankame <handycola3@gmail.com>
2016-12-29 02:57:56 +0900

    テストで静的ファイルが読み込まれるところまで確認。

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,6 +6,8 @@ let router = Router()
 
 router.add(templateEngine: StencilTemplateEngine())
 
+router.all("/", middleware: StaticFileServer(path: "./Views/Public"))
+
 //Handle HTTP GET requests to Stencil
 router.get("/") { request, response, next in
     defer {
@@ -28,7 +30,7 @@ router.get("/") { request, response, next in
 }
 
 // Add an HTTP server and connect it to the router
-Kitura.addHTTPServer(onPort: 80, with: router)
+Kitura.addHTTPServer(onPort: 8090, with: router)
 
 // Start the Kitura runloop (this call never returns)
 Kitura.run()
