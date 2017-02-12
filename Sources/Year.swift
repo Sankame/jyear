e225d4d5759bc8b404f6b5c5954e0c3b250713b9
sankame <handycola3@gmail.com>
2017-02-12 21:12:43 +0900

    年間の日にちと曜日のリストを作成まで。

--- Sources/Year.swift
+++ Sources/Year.swift
@@ -8,14 +8,18 @@ class Year{
     private var year:Int = 0
     
     init(year:Int){
+        var datesOfYear = [Dictionary<String,String>]()
+        
         self.year = year
         
-        let month:Month = Month(year: self.year, month: 2, start: 1, end: 28)
-        let dates = month.getDates()
+        (1...12).forEach{
+            let endDate = self.getEndByMonth(year: self.year, month: $0)
+            let month:Month = Month(year: self.year, month: $0, start: 1, end: endDate)
+            datesOfYear = datesOfYear + month.getDates()
+        }
         
-        for eachDate in dates{
+        for eachDate in datesOfYear{
             print(eachDate["date"]! + "/" + eachDate["day"]!)
-            //log.verbose(eachDate["date"]! + "/" + eachDate["day"]!)
         }
     }
     
@@ -23,5 +27,32 @@ class Year{
 //        return self.dates
 //    }
     
+    private func getEndByMonth(year:Int, month:Int)->Int{
+        switch month {
+        case 2:
+            if self.isLeapYear(year: year){
+                return 29
+            }else{
+                return 28
+            }
+        case 4, 6, 9, 11:
+            return 30
+        default:
+            return 31
+        }
+    }
+    
+    private func isLeapYear(year: Int) -> Bool {
+        // 4以上ではない場合は弾く
+        guard year >= 4 else {
+            return false
+        }
+        
+        // 閏年の条件
+        // - 4で割り切れる年は閏年
+        // - 100で割り切れる年は閏年ではない
+        // - 400で割り切れる年は閏年
+        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)
+    }
 }
 
