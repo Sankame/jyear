e49c10e1a21d40bbe3eba39cd562bed301b0eaf2
allabout <system+github@corp.allabout.co.jp>
2017-02-16 01:07:07 +0900

    カレンダーデータを作ったところまで。

--- Sources/Year.swift
+++ Sources/Year.swift
@@ -7,7 +7,7 @@ class Year{
     //年
     private var year:Int = 0
     
-    private var datesOfYear = [Dictionary<String,String>]()
+    //private var datesOfYear = [Dictionary<String,String>]()
     
     private var datesByMonth:Dictionary<String, Array<Dictionary<String,String>>> = [:]
     
@@ -19,9 +19,9 @@ class Year{
         (1...12).forEach{
             let endDate = self.getEndByMonth(year: self.year, month: $0)
             let month:Month = Month(year: self.year, month: $0, start: 1, end: endDate)
-            datesOfYear = datesOfYear + month.getDates()
+//            datesOfYear = datesOfYear + month.getDates()
             
-            self.datesByMonth[String($0)] = month.getDates()
+            self.datesByMonth[String($0)] = self.makeCalData(datesOfMonth: month.getDates())
         }
 
 // TODO,debug
@@ -35,6 +35,47 @@ class Year{
 //        datesForReturn["test"] = self.datesOfYear
 //        return datesForReturn
         return self.datesByMonth
+    }
+    
+    private func makeCalData(datesOfMonth:Array<Dictionary<String,String>>)->Array<Dictionary<String,String>>{
+        
+        var calData = [Dictionary<String,String>]()
+        
+        let days = ["日", "月", "火", "水", "木", "金", "土"]
+        var calIndex = 0
+        var monthIndex = 0
+
+        week: for _ in 1...10{
+        
+            for day in days{
+
+                var item:Dictionary<String, String> = [:]
+                item["index"] = String(calIndex)
+
+                // カレンダーとその月の曜日が一致する間だけ日にちをセット。
+                if monthIndex < datesOfMonth.count
+                    && day == datesOfMonth[monthIndex]["day"]{
+
+                    item["date"] = datesOfMonth[monthIndex]["date"]
+                    item["day"] = datesOfMonth[monthIndex]["day"]
+                    
+                    monthIndex = monthIndex+1
+
+                }else{
+                    item["date"] = ""
+                    item["day"] = ""
+                }
+                
+                calData.append(item)
+                calIndex = calIndex + 1
+                
+                if day == days[days.count-1] && item["date"] == ""{
+                    break week
+                }
+            }
+
+        }
+        return calData
     }
     
     private func getEndByMonth(year:Int, month:Int)->Int{
