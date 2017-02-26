a9f5f41195710b753468b7eb97b0b111a9c84cbe
sankame <handycola3@gmail.com>
2017-02-26 23:26:43 +0900

    webpackでJSの結合を行ったところまで。

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+context: './Views/Public/simple-cal/js',
+
+entry: {
+		js: "./main.js"
+},
+
+output: {
+		path: './Views/Public/simple-cal/js',
+		filename: "bundle.js"
+}
+
+}
+
