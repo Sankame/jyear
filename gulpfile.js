c85cb8cd695f08670ed6175c977baa0b6d68c72d
sankame <handycola3@gmail.com>
2017-03-02 23:05:29 +0900

    gulpでbundleしたファイルを三分割。

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,37 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 
 // jsタスクを定義する
-gulp.task('js', function () {
+gulp.task('js1', function () {
   // タスクを実行するグロブを指定
   gulp.src([
   	'./Views/Public/simple-cal/js/modernizr-2.6.2.min.js'
   	,'./Views/Public/simple-cal/js/respond.min.js'
   	,'./Views/Public/simple-cal/js/jquery.min.js'
-  	,'./Views/Public/simple-cal/js/jquery.easing.1.3.js'
+  	])
+    // 実行する処理を実行する順にpipeでつないでいく
+    .pipe(concat('bundle1.js')) //ファイルを結合し、bundle.jsファイルとして出力
+    .pipe(uglify({preserveComments: 'some'})) // ファイルを圧縮する（ライセンス情報は外す）
+    .pipe(gulp.dest('./Views/Public/simple-cal/js/dist')); //distディレクトリに出力
+});
+
+// jsタスクを定義する
+gulp.task('js2', function () {
+  // タスクを実行するグロブを指定
+  gulp.src([
+  	'./Views/Public/simple-cal/js/jquery.easing.1.3.js'
   	,'./Views/Public/simple-cal/js/bootstrap.min.js'
-  	,'./Views/Public/simple-cal/js/owl.carousel.min.js'
+  	])
+    // 実行する処理を実行する順にpipeでつないでいく
+    .pipe(concat('bundle2.js')) //ファイルを結合し、bundle.jsファイルとして出力
+    .pipe(uglify({preserveComments: 'some'})) // ファイルを圧縮する（ライセンス情報は外す）
+    .pipe(gulp.dest('./Views/Public/simple-cal/js/dist')); //distディレクトリに出力
+});
+
+// jsタスクを定義する
+gulp.task('js3', function () {
+  // タスクを実行するグロブを指定
+  gulp.src([
+  	'./Views/Public/simple-cal/js/owl.carousel.min.js'
   	,'./Views/Public/simple-cal/js/jquery.stellar.min.js'
   	,'./Views/Public/simple-cal/js/jquery.waypoints.min.js'
   	,'./Views/Public/simple-cal/js/jquery.countTo.js'
@@ -22,24 +44,40 @@ gulp.task('js', function () {
   	,'./Views/Public/simple-cal/js/simple-cal.js'
   	])
     // 実行する処理を実行する順にpipeでつないでいく
-    .pipe(concat('bundle.js')) //ファイルを結合し、bundle.jsファイルとして出力
+    .pipe(concat('bundle3.js')) //ファイルを結合し、bundle.jsファイルとして出力
     .pipe(uglify({preserveComments: 'some'})) // ファイルを圧縮する（ライセンス情報は外す）
     .pipe(gulp.dest('./Views/Public/simple-cal/js/dist')); //distディレクトリに出力
 });
 
-gulp.task('css', function() {
+gulp.task('css1', function() {
   return gulp.src([
           './Views/Public/simple-cal/css/animate.css'
           ,'./Views/Public/simple-cal/css/icomoon.css'
-          ,'./Views/Public/simple-cal/css/bootstrap.css'
-          ,'./Views/Public/simple-cal/css/owl.carousel.min.css'
+      ])
+    .pipe(concat('bundle1.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./Views/Public/simple-cal/css/dist'));
+});
+
+gulp.task('css2', function() {
+  return gulp.src([
+  		  './Views/Public/simple-cal/css/bootstrap.css'
+      ])
+    .pipe(concat('bundle2.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./Views/Public/simple-cal/css/dist'));
+});
+
+gulp.task('css3', function() {
+  return gulp.src([
+  		  './Views/Public/simple-cal/css/owl.carousel.min.css'
           ,'./Views/Public/simple-cal/css/owl.theme.default.min.css'
           ,'./Views/Public/simple-cal/css/style.css'
       ])
-    .pipe(concat('bundle.css'))
+    .pipe(concat('bundle3.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('./Views/Public/simple-cal/css/dist'));
 });
 
 // jsタスクをdefaultタスクとして登録
-gulp.task('default', ['js','css']);
+gulp.task('default', ['js1','js2','js3','css1','css2','css3']);
